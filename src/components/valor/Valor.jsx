5149606114b57b003591b855cffd7f7fe28654a0
Matheus Magalhães Urbano <89428438+magalhaesmth@users.noreply.github.com>
2022-07-14 20:52:51 -0300

    Implementando handleChange e handleSubmit

--- src/components/valor/Valor.jsx
+++ src/components/valor/Valor.jsx
@@ -4,15 +4,33 @@ import './Valor.css';
 class Valor extends React.Component {
 
     constructor(props) {
-        super(props);         
+        super(props);
+        this.state = { valor: "" }
+            
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);      
     }
+
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        let valor = this.state.valor;
+        this.setState({
+            valor: valor
+        }
+    )};
 
     render() {
         return (
             <div class="valor">
+                <form onSubmit={this.handleSubmit}>
                 <h2>Valor:</h2>
-                <input type="number" class="input_valor" name="valor" id="valor" placeholder="Ex: 1500,00" required /><br/>
+                    <input type="number" class="input_valor" name="valor" id="valor" placeholder="Ex: 1500,00" value={this.state.input_valor} onChange={this.handleChange} required/><br/>
                 <p>Valor Recebido</p>
+                </form>
             </div>
         );
     }
