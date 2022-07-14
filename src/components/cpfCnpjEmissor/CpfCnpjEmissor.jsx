2dc890f3966691a54625ac42781104fc432ebf53
Matheus Magalhães Urbano <89428438+magalhaesmth@users.noreply.github.com>
2022-07-14 20:50:38 -0300

    Implementando handleChange e handleSubmit

--- src/components/cpfCnpjEmissor/CpfCnpjEmissor.jsx
+++ src/components/cpfCnpjEmissor/CpfCnpjEmissor.jsx
@@ -5,13 +5,30 @@ class CpfCnpjEmissor extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = { cpfCnpjEmissor: "" }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        let cpfCnpjEmissor = this.state.cpfCnpjEmissor;
+        this.setState({
+            cpfCnpjEmissor: cpfCnpjEmissor
+        }
+        )
+    };
 
     render() {
         return (
             <div class="cpfCnpjEmissor">
                 <h2>CPF ou CNPJ (Opcional):</h2>
-                <input type="text"  name="cpfCnpjEmissor" id="cpfCnpjEmissor" placeholder="Ex: 000.000.000-00"/><br/>
+                <input type="text" name="cpfCnpjEmissor" class="input_cpfCnpjEmissor" id="cpfCnpjEmissor" placeholder="Ex: 000.000.000-00" value={this.state.input_cpfCnpjEmissor} onChange={this.handleChange} required/><br/>
                 <p>Somente números</p>
             </div>
         );
